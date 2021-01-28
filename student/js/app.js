741be348bfa5e2d5084394192db88228fd626c2c
Md. Toufiqul Islam <toufiq.austcse@gmail.com>
2021-01-28 11:46:06 +0600

    Changes added

--- student/js/app.js
+++ student/js/app.js
@@ -1,161 +1,157 @@
 (function () {
-	/* Canvas */
-	let currentPage = 1;
+	let uuid = '2558966';
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", 'https://stage-live.10minuteschool.com/api/v1/class/admin/config', true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify({
+		class_id: "1",
+		user_name: "sadi",
+		uuid: uuid
 
-	var canvas = document.getElementById('drawCanvas');
-	var ctx = canvas.getContext('2d');
-	//canvas.width = Math.min(document.documentElement.clientWidth, window.innerWidth || 300);
-	//canvas.height = Math.min(document.documentElement.clientHeight, window.innerHeight || 300);
-	canvas.width = 800;
-	canvas.height = 600;
+	}));
 
+	xhr.onreadystatechange = function () {
+		if (this.readyState != 4) return;
 
-	ctx.lineWidth = '3';
-	ctx.lineCap = ctx.lineJoin = 'round';
+		if (this.status == 200) {
+			var res = JSON.parse(this.responseText);
+			console.log(res);
+			let { agora_config, channel_list, pubnub_config } = res.data[0];
 
-	let baseImg = new Image();
-
-
-	/* load image */
-	function loadImage(page) {
-		baseImg.src = `https://uadoc.uacdn.net/live_class/CWFZUJ2N/1602882378I9PSYAHO.pdf?page=${page}&fm=webp&fit=clip&auto=compress&w=1080`;
-
-		baseImg.onload = function () {
-			ctx.drawImage(baseImg, 0, 0, baseImg.width, baseImg.height, 0, 0, canvas.width, canvas.height);
-		}
-
-	}
-
-	loadImage(currentPage);
+			/* Canvas */
+			let currentPage = 1;
+			var canvas = document.getElementById('drawCanvas');
+			var ctx = canvas.getContext('2d');
+			canvas.width = 800;
+			canvas.height = 600;
 
 
-	/* Mouse and touch events */
+			ctx.lineWidth = '3';
+			ctx.lineCap = ctx.lineJoin = 'round';
 
-	document.getElementById('colorSwatch').addEventListener('click', function () {
-		color = document.querySelector(':checked').getAttribute('data-color');
-	}, false);
-
-	/* var isTouchSupported = 'ontouchstart' in window;
-	var isPointerSupported = navigator.pointerEnabled;
-	var isMSPointerSupported = navigator.msPointerEnabled;
-
-	var downEvent = isTouchSupported ? 'touchstart' : (isPointerSupported ? 'pointerdown' : (isMSPointerSupported ? 'MSPointerDown' : 'mousedown'));
-	var moveEvent = isTouchSupported ? 'touchmove' : (isPointerSupported ? 'pointermove' : (isMSPointerSupported ? 'MSPointerMove' : 'mousemove'));
-	var upEvent = isTouchSupported ? 'touchend' : (isPointerSupported ? 'pointerup' : (isMSPointerSupported ? 'MSPointerUp' : 'mouseup'));
-
-	canvas.addEventListener(downEvent, startDraw, false);
-	canvas.addEventListener(moveEvent, draw, false);
-	canvas.addEventListener(upEvent, endDraw, false); */
-
-	/* PubNub */
-
-	var channel = 'draw';
+			let baseImg = new Image();
 
 
-	var pubnub = PUBNUB.init({
-		publish_key: 'pub-c-73c58b0d-5c27-4e4b-bb58-377024196a3c',
-		subscribe_key: 'sub-c-4baa524e-5888-11eb-95c0-3253a07b53cf',
-		leave_on_unload: true,
-		ssl: document.location.protocol === "https:"
-	});
+			/* load image */
+			function loadImage(page) {
+				baseImg.src = `https://uadoc.uacdn.net/live_class/CWFZUJ2N/1602882378I9PSYAHO.pdf?page=${page}&fm=webp&fit=clip&auto=compress&w=1080`;
 
-	pubnub.subscribe({
-		channel: channel,
-		callback: drawFromStream,
-		presence: function (m) {
-			if (m.occupancy > 1) {
-				document.getElementById('unit').textContent = 'doodlers';
+				baseImg.onload = function () {
+					ctx.drawImage(baseImg, 0, 0, baseImg.width, baseImg.height, 0, 0, canvas.width, canvas.height);
+				}
+
 			}
-			document.getElementById('occupancy').textContent = m.occupancy;
-			var p = document.getElementById('occupancy').parentNode;
-			p.classList.add('anim');
-			p.addEventListener('transitionend', function () { p.classList.remove('anim'); }, false);
-		}
-	});
 
-	/* function publish(data) {
-		pubnub.publish({
-			channel: channel,
-			message: data
-		});
-	} */
 
-	/* Draw on canvas */
 
-	function drawOnCanvas(color, plots) {
-		ctx.strokeStyle = color;
-		ctx.beginPath();
-		ctx.moveTo(plots[0].x, plots[0].y);
 
-		for (var i = 1; i < plots.length; i++) {
-			ctx.lineTo(plots[i].x, plots[i].y);
-		}
-		ctx.stroke();
-	}
+			/* Mouse and touch events */
 
-	function drawFromStream(message) {
-		if (message.page) {
-			loadImage(message.page)
-		} else {
-			if (!message || message.plots.length < 1) return;
-			drawOnCanvas(message.color, message.plots);
-		}
+			document.getElementById('colorSwatch').addEventListener('click', function () {
+				color = document.querySelector(':checked').getAttribute('data-color');
+			}, false);
 
-	}
 
-	// Get Older and Past Drawings!
-	if (drawHistory) {
-		pubnub.history({
-			channel: channel,
-			count: 50,
-			callback: function (messages) {
-				pubnub.each(messages[0], drawFromStream);
+			/* PubNub */
+
+			var drawChannel = channel_list.drawing;
+			let pageChannel = channel_list.page;
+
+
+			var pubnub = PUBNUB.init({
+				publish_key: pubnub_config.pub_key,
+				subscribe_key: pubnub_config.sub_key,
+				leave_on_unload: true,
+				ssl: document.location.protocol === "https:",
+				uuid: uuid,
+				authKey: pubnub_config.auth_key
+
+			});
+
+			pubnub.subscribe({
+				channel: drawChannel,
+				callback: drawFromStream,
+				presence: function (m) {
+					if (m.occupancy > 1) {
+						document.getElementById('unit').textContent = 'doodlers';
+					}
+					document.getElementById('occupancy').textContent = m.occupancy;
+					var p = document.getElementById('occupancy').parentNode;
+					p.classList.add('anim');
+					p.addEventListener('transitionend', function () { p.classList.remove('anim'); }, false);
+				}
+			});
+
+			pubnub.history(
+				{
+					channel: pageChannel,
+					count: 1, // how many items to fetch
+					stringifiedTimeToken: true, // false is the default
+				},
+				function (status, response) {
+					if (status.length > 0) {
+						console.log(status);
+						if (status[0][0].page) {
+							currentPage = status[0][0].page
+							loadImage(currentPage);
+						} else {
+							loadImage(currentPage);
+						}
+					}
+					// handle status, response
+				}
+			);
+
+			/* function publish(data) {
+				pubnub.publish({
+					channel: channel,
+					message: data
+				});
+			} */
+
+			/* Draw on canvas */
+
+			function drawOnCanvas(color, plots) {
+				ctx.strokeStyle = color;
+				ctx.beginPath();
+				ctx.moveTo(plots[0].x, plots[0].y);
+
+				for (var i = 1; i < plots.length; i++) {
+					ctx.lineTo(plots[i].x, plots[i].y);
+				}
+				ctx.stroke();
 			}
-		});
-	}
-	var isActive = false;
-	var plots = [];
 
-	function draw(e) {
-		e.preventDefault(); // prevent continuous touch event process e.g. scrolling!
-		if (!isActive) return;
+			function drawFromStream(message) {
+				if (message.page) {
+					loadImage(message.page)
+				} else {
+					if (!message || message.plots.length < 1) return;
+					drawOnCanvas(message.color, message.plots);
+				}
 
-		var x = isTouchSupported ? (e.targetTouches[0].pageX - canvas.offsetLeft) : (e.offsetX || e.layerX - canvas.offsetLeft);
-		var y = isTouchSupported ? (e.targetTouches[0].pageY - canvas.offsetTop) : (e.offsetY || e.layerY - canvas.offsetTop);
+			}
 
-		plots.push({ x: (x << 0), y: (y << 0) }); // round numbers for touch screens
+			// Get Older and Past Drawings!
+			if (drawHistory) {
+				pubnub.history({
+					channel: channel,
+					count: 50,
+					callback: function (messages) {
+						pubnub.each(messages[0], drawFromStream);
+					}
+				});
+			}
 
-		drawOnCanvas(color, plots);
-	}
 
-	function startDraw(e) {
-		e.preventDefault();
-		isActive = true;
-	}
 
-	function endDraw(e) {
-		e.preventDefault();
-		isActive = false;
 
-		publish({
-			color: color,
-			plots: plots
-		});
-
-		plots = [];
-	}
-
-	/* document.addEventListener('keydown', e => {
-		if (e.key === 'ArrowRight') {
-			loadImage(++currentPage);
-			publish({
-				page: currentPage
-			});
-		} else if (e.key === 'ArrowLeft') {
-			loadImage(--currentPage);
-			publish({
-				page: currentPage
-			});
+			// we get the returned data
 		}
-	})  */
+
+		// end of state change: it can be after some time (async)
+	};
+
+
+
 })();
